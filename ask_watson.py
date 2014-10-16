71066d9b65169c36503cf59d604873af5fd70691
Alexander M. Shoup <shoup_alex@yahoo.com>
2014-10-15 20:44:10 -0400

    Added watson interface

---
+++ ask_watson.py
@@ -0,0 +1,23 @@
+import httplib, urllib
+import json
+
+class Watson:
+    WATSON_INTERFACE_URI = "ghc47.ghc.andrew.cmu.edu"
+    WATSON_INTERFACE_PORT = 8070
+    watson_conn = httplib.HTTPConnection(WATSON_INTERFACE_URI, WATSON_INTERFACE_PORT)
+
+    def ask(self, question):
+        self.watson_conn.request("GET", "/qa/answer?q=" + question)
+        return self.watson_conn.getresponse()
+
+if __name__ == "__main__":
+    #example use case
+    watson = Watson()
+    q = urllib.quote('What is Carnegie Mellon University?')
+    res = watson.ask(q)
+    print res.status, res.reason
+    text = res.read()
+    json_res = json.loads(text)
+
+    #pretty print
+    print json.dumps(json_res, indent=4, separators=(',', ': '))
